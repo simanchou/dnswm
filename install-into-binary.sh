35b621e5ae5241da2495acc54eb98a82111904e8
siman <siman@office.vm>
2019-09-19 15:01:54 +0800

    fix bug for install shell

--- install-into-binary.sh
+++ install-into-binary.sh
@@ -1,18 +1,32 @@
+VERSION="1.0"
+PKG_MD5="48eaa9371a2571ed62188a6ccc041423"
 export DATA_PATH=/opt/dnswm
 export DNSWM_PORT=9001
+useradd -M -s /sbin/nologin dnswm
 mkdir -p ${DATA_PATH}/zones
 mkdir -p ${DATA_PATH}/bin
-curl -L https://github.com/simanchou/dnswm/releases/download/v1.0/dnswm-linux-1.0.tar.gz -o ${DATA_PATH}/dnswm-linux-1.0.tar.gz
-cd ${DATA_PATH}
-tar zxf dnswm-linux-1.0.tar.gz
-chmod +x dnswm
-chmod +x coredns
-mv coredns ${DATA_PATH}/bin/coredns
-mv dnswm ${DATA_PATH}/bin/dnswm
-mv assets ${DATA_PATH}/bin/assets
-mv tmpl ${DATA_PATH}/bin/tmpl
+
+if [[ ! -f /tmp/dnswm-linux-${VERSION}.tar.gz ]];then
+    echo "begin to download tar file dnswm-linux-${VERSION}.tar.gz"
+    echo "if download fail, try again or download yourself and upload to /tmp/dnswm-linux-${VERSION}.tar.gz"
+    curl -L https://github.com/simanchou/dnswm/releases/download/v${VERSION}/dnswm-linux-${VERSION}.tar.gz -o /tmp/dnswm-linux-${VERSION}.tar.gz
+else
+    echo "dnswm-linux-${VERSION}.tar.gz already exist"
+    echo "begin to check md5 of dnswm-linux-${VERSION}.tar.gz"
+    EXIST_FILE_MD5=$(md5sum /tmp/dnswm-linux-${VERSION}.tar.gz|awk '{print $1}')
+    if [[ "$EXIST_FILE_MD5" == "$PKG_MD5" ]];then
+        echo "tar file validate successful"
+        echo "begin to install dnswm"
+    else
+        echo "file validate fail.try again, or download yourself and upload to /tmp/dnswm-linux-${VERSION}.tar.gz"
+        echo "download url: https://github.com/simanchou/dnswm/releases/download/v${VERSION}/dnswm-linux-${VERSION}.tar.gz"
+        rm -rf /tmp/dnswm-linux-${VERSION}.tar.gz
+        exit 1
+    fi
+fi
+
+tar zxf /tmp/dnswm-linux-${VERSION}.tar.gz -C ${DATA_PATH}/bin
 export DNS_SERVER_IP=$(hostname -I | cut -d" " -f 1)
-useradd -M -s /sbin/nologin dnswm
 
 cat >/etc/systemd/system/coredns.service <<EOF
 [Unit]
@@ -84,8 +98,49 @@ lan {
 }
 EOF
 
+chown -R dnswm:dnswm ${DATA_PATH}
 
+echo "finish install, start service"
 systemctl daemon-reload
 systemctl restart coredns
 systemctl restart dnswm
 
+systemctl enable coredns
+systemctl enable dnswm
+
+DNS_SERVICE_IS_ACTIVE=1
+DNSWM_SERVICE_IS_ACTIVE=1
+ss -utnlp|grep 53
+DNS_PROC=$(systemctl is-active coredns)
+if [[ "${DNS_PROC}" == "active" ]];then
+    printf "\e[34mdns service is Fine! \e[0m\n"
+else
+    DNS_SERVICE_IS_ACTIVE=0
+    printf "\e[33mWARNING!!! dns service is NOT Running! \e[0m\n"
+fi
+
+ss -utnlp|grep 9001
+DNSWM_PROC=$(systemctl is-active dnswm)
+if [[ "${DNSWM_PROC}" == "active" ]];then
+    printf "\e[34mdnswm service is Fine! \e[0m\n"
+else
+    DNSWM_SERVICE_IS_ACTIVE=0
+    printf "\e[33mWARNING!!! dnswm service is NOT Running! \e[0m\n"
+fi
+
+echo
+if [[ ${DNS_SERVICE_IS_ACTIVE} -eq 1 ]] && [[ ${DNSWM_SERVICE_IS_ACTIVE} -eq 1 ]]; then
+printf "
+    install successful, then you can visit http://${DNS_SERVER_IP}:9001 to add some domain
+    and check by \"nslookup *** ${DNS_SERVER_IP}\"\n\n \
+    BTW, you should make sure open the port 53/udp and 9001/tcp in your firewall\n\n \
+    service manage:\n \
+    systemctl status|start|stop|restart dnswm\n \
+    systemctl status|start|stop|restart coredns\n\n"
+else
+    echo "install fail"
+    echo "do some clean job, please waite..."
+    echo "delete \"dnswm\" user"
+    userdel dnswm
+    echo "clean job done"
+fi
